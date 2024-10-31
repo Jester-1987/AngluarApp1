aff37f67056e0c7eab15fe67148c9842bc98b676
Jester-1987 <nicole.groeger@triosstudent.com>
2024-10-31 09:13:17 -0400

    completed project

--- ReservationStation/src/app/app.component.ts
+++ ReservationStation/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StoreModule } from './store/store.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
---
+++ ReservationStation/src/app/model/product.model.ts
@@ -0,0 +1,8 @@
+export class Product {
+    constructor(
+        public id?: number,
+        public area?: string,
+        public description?: string,
+        public timeSlot?: number,
+        public price?: number) { }
+}
